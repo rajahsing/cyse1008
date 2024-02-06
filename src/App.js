af56ddab89a335f00ecfb087f86de1eb9bb1bb32
rajahsing <rajaharindersingh@loyalistcollege.com>
2024-02-06 08:21:41 -0500

    Changed app.js file with name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Cliff Coulter
         </a>
       </header>
     </div>
